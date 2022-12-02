625bb3cf6007452f400690a578323e4c29e0e209
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-12-02 07:26:21 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * is_palindrome - check is a linked list is palindrome
- * @head: head of the list
- * Return: 0 if not 1 if it is
- */
+* is_palindrome - check is a linked list is palindrome
+* @head: head of the list
+* Return: 0 if not 1 if it is
+*/
 
 int is_palindrome(listint_t **head)
 {
