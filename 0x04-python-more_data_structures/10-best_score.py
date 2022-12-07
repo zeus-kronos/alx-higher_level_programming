35e71cf236c61e8685c3550324113fbf376ae346
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-12-07 20:37:31 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
-        return None
-
-    current_high_score = 0
-    high_score_holder = None
-
-    for key in a_dictionary:
-        if a_dictionary[key] > current_high_score:
-            current_high_score = a_dictionary[key]
-            high_score_holder =  key
-
-    return high_score_holder
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
